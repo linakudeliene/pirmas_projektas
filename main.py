22247bd6afebe6aeeaa2f3ecea46964842fc9663
linakudeliene <kudeliene@gmail.com>
2023-09-10 09:12:09 +0300

    pirmas

---
+++ main.py
@@ -0,0 +1,75 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# skaicius = int(input("iveskite skaiciu : "))
+# suma = 0
+#
+# for i in range(1, skaicius +1):
+#     suma += i
+# print("skaiciu suma nuo 1 iki", skaicius, "yra", suma )
+
+# eiluciu_sk = int(input("iveskite sveika skaiciu" ))
+# for eilute in range(1, eiluciu_sk+1):
+#     tarpas = eiluciu_sk - eilute
+#     luna = 2 * eilute -1
+#     print(" "* tarpas + "*" * luna)
+
+# zodziai_is_a = ["as", "tu", "jis", "asta", "marius"]
+# raide = "a"
+# for zodis in zodziai_is_a:
+#     if zodis[0].lower()== raide.lower():
+#         print(zodis)
+
+# zodynas = ["jonas", "petras", "milda", "migle"]
+# tekstas = ["jonas ir petras buvo broliai ju kaimynes buvo migle ir milda.jonas ir milda lanke ta pacia mokykla, jonas buvo penktokas, o milda trecioke"]
+# for zodis in zodynas:
+#     if zodis[0]==tekstas*3:
+#         print(zodis)
+
+# mokinio_pazymys = int(input("iveskite_pazymi"))
+#
+# if mokinio_pazymys <= 2 :
+#     print( "Neuztektinai" )
+# elif mokinio_pazymys <= 5:
+#     print( "Silpnai" )
+# elif mokinio_pazymys <= 7:
+#     print( "vidutiniskai" )
+# elif mokinio_pazymys <= 9 :
+#     print( "gerai" )
+# else:
+#     print( "puikiai" )
+
+# kelimieji_metai = int(input("iveskite metus"))
+# if kelimieji_metai %4 == 0:
+#     print("keliamieji metai")
+# else :
+#     print("nekeliamieji metai")
+
+zodziai = [
+    'jonas':22,
+    'asta':17,
+    'marius':20
+]
+zodynas = {}
+for amzius in zodynas.items():
+    zodynas[amzius] = int
+for amzius in zodynas.items():
+    if amzius >= 18:
+        print("amzius didesnis arba lygus ", zodziai)
+
+
+
